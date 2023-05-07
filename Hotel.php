7966c0f0045f38e6cae0d7e4a3f63abd93fb90a3
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-05-07 17:55:01 +0200

    delete some spaces

--- Hotel.php
+++ Hotel.php
@@ -1,9 +1,5 @@
 <?php
 
-// Ajouter fct si chambre réservée on ne peut pas la réserver.
-// Ajouter fct où je peux annuler la resa et ça repasse en dispo.
-// Annuler une resa refresh le tableau d'affichage.
-
 class Hotel {
 
     private string $name;
@@ -12,18 +8,15 @@ class Hotel {
     private string $city;
     private array $rooms; // Array contenant les différentes chambres/objet
     private int $nbRoomsReserved; // nbr de chambres réservées
-    private array $reservations;
-    
+    private array $reservations;    
 
-    public function __construct(string $name, string $address, string $cp, string $city) {
-
+    public function __construct(string $name, string $address, string $cp, string $city) {        
         $this->name = $name;
         $this->address = $address;
         $this->cp = $cp;
         $this->city = $city;
         $this->rooms = array(); // Initialisation d'un array Chambres qui contiendra la liste des chambres. les items Chambres/Room.   
-        $this->reservations = array();      
-
+        $this->reservations = array();     
     }
 
     public function addResa($reservation){
@@ -49,7 +42,6 @@ class Hotel {
         $numberOfRooms = count($this->rooms); // On compte le nombre d'items dans l'array Rooms.
 
         return ($numberOfRooms + 1); // On retourne ce nombre + 1 afin de retourner le nouveau numéro de chambre.
-
     }
 
     public function getInfosHotel(){
@@ -64,7 +56,6 @@ class Hotel {
         "Nombre de chambres : ".$nbRoom."<br>". 
         "Nombre de chambres réservées : ".$nbResa."<br>". 
         "Nombre de chambres disponibles : ".$nbRoomAvailable; // Compte le nbr de chambres avec count($array)
-
     }
 
     public function getReservationsList(){   
@@ -95,13 +86,10 @@ class Hotel {
 
     }
 
-    public function getStatutRoom(){ // Affichage en bootstrap de la liste des chambres et leurs features dans l'hotel.
-
-        
+    public function getStatutRoom(){ // Affichage en bootstrap de la liste des chambres et leurs features dans l'hotel.        
 
         // $content représente le contenu qui sera affiché en boostrap.
         // Dans un 1er temps on ajoute à $content les différentes colonnes de la table.
-
         
         $content = "
             <table class='table table-white'>
